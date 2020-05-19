480a6262b4417a4e157e903dd0b7345976bacef5
ponjoru <igorpopov99554@gmail.com>
2020-05-19 15:02:33 +0300

    Optimizers bottleneck refactored. No redundant optimizers instances should be spawned.

--- utils/optimizer.py
+++ utils/optimizer.py
@@ -15,21 +15,23 @@ def define_optimizer(model: nn.Module, opt_name: str, opt_params: dict = None):
         param_list = module.get_params()
     else:
         param_list = module.params()
-    if opt_params is not None:
-        optimizers = {
-            'sgd': SGD(param_list, **opt_params),
-            'adam': Adam(param_list, **opt_params),
-            'adadelta': Adadelta(param_list, **opt_params),
-            'rmsprop': RMSprop(param_list, **opt_params)
-        }
-    else:
-        optimizers = {
-            'sgd': SGD(param_list, lr=0.001, momentum=0, dampening=0, weight_decay=0, nesterov=False),
-            'adam': Adam(param_list, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False),
-            'adadelta': Adadelta(param_list, lr=1.0, rho=0.9, eps=1e-06, weight_decay=0),
-            'rmsprop': RMSprop(param_list, lr=0.01, alpha=0.99, eps=1e-08, weight_decay=0, momentum=0, centered=False)
-        }
+
+    optimizers = {
+        'sgd': SGD,
+        'adam': Adam,
+        'adadelta': Adadelta,
+        'rmsprop': RMSprop
+    }
+    default_params = {
+        'sgd': {"lr": 0.001, "momentum": 0, "dampening": 0, "weight_decay": 0, "nesterov": False},
+        'adam': {"lr": 0.001, "betas": (0.9, 0.999), "eps": 1e-08, "weight_decay": 0, "amsgrad": False},
+        'adadelta': {"lr": 1.0, "rho": 0.9, "eps": 1e-06, "weight_decay": 0},
+        'rmsprop': {"lr": 0.01, "alpha": 0.99, "eps": 1e-08, "weight_decay": 0, "momentum": 0, "centered": False}
+    }
     if opt_name not in optimizers:
         raise NotImplementedError(''.join(optimizers.keys()) + ': optimizers are available, given %s' % opt_name)
 
-    return optimizers[opt_name]
+    if opt_params is None:
+        opt_params = default_params[opt_name]
+
+    return optimizers[opt_name](param_list, **opt_params)
